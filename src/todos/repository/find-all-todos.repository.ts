e663acf6719a2b64fc4637152918fd608006b604
PatoCalvo38 <lorenzo.martins@estudante.ifms.edu.br>
2026-03-06 10:54:46 -0400

    algumacoisa

---
+++ src/todos/repository/find-all-todos.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared databases/databases/prisma.database";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+
+@Injectable()
+export class FindAllTodosRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async execute() {
+        return await this.prisma.todo.findMany({})
+    }
+}
---
+++ src/todos/repository/find-todo-by-id.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared databases/databases/prisma.database";
+
+
+@Injectable()
+export class FindTodoByIdRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async execute(id: string) {
+        return await this.prisma.todo.findUnique({ id })
+    }
+}
